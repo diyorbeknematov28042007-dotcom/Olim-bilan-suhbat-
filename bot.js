5b8a8d706e1ff391facffe8c36743329bc6589c5
diyorbeknematov28042007-dotcom <diyorbeknematov28042007@gmail.com>
2026-05-06 22:57:20 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -455,7 +455,7 @@ Olim sifatida muloyim, donishmand, tajribali ustozday javob ber. Huquq, kriminal
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
